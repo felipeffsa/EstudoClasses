311f4da5bd72f6269a23170a9c60192dcc5b1c05
Felipe <felipeffsa@hotmail.com>
2024-02-16 15:38:53 -0300

    Estudo basico concluido

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from usuario.views import PessoasListView,PessoasDetailView
+from usuario.views import PessoasListView,PessoasDetailView,CriarVisu,AtualizarCadastro,DeletarCadastro
 
 
 urlpatterns = [
     path('pessoas/',PessoasListView.as_view(), name ='pessoas'),
-    path('detalhes/<int:pk>/',PessoasDetailView.as_view(), name ='detail')
+    path('detalhes/<int:pk>/',PessoasDetailView.as_view(), name ='detail'),
+    path('criar/',CriarVisu.as_view(), name ='criar'),
+    path('atualizar/<int:pk>/',AtualizarCadastro.as_view(),name ='atualizar' ),
+    path('deletar/<int:pk>/',DeletarCadastro.as_view(), name ='deletar')
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView, CreateView,UpdateView,DeleteView
 from usuario.models import Pessoas
-
+from django.urls import reverse_lazy
 # Create your views here.
 class PessoasListView(ListView):
     model = Pessoas
@@ -14,3 +14,27 @@ class PessoasDetailView(DetailView):
     model = Pessoas
     template_name ='detalhes.html'
     context_object_name ='detail'
+    
+# Classe para criação de cadastro
+class CriarVisu(CreateView): # Essa é uma view utilizada para criar uma nova pessoa no cadastro. Ela deve herdar de CreateView.
+    model = Pessoas  # Passamos a classe na qual queremos criar um novo cadastro.
+    fields = ['nome','idade','nacionalidade','profissao'] # Aqui será passado os campos que nós queremos que apareça no formulário.
+    template_name = 'criar.html' # O template contendo o formulário com o metodo POST.
+    success_url = '/pessoas/' # A url que será redirecionada, quando acontecer o POST.
+    def form_valid(self,form): # Essa função irá verificar se o formulario é valido. Ela é semelhante a do ModelForm. 'form.is_valid()'.
+        return super().form_valid(form) # Utilizamos o 'super()' para pegar a função da classe pai.
+                                        # Classe para a atualização do cadastro
+
+class AtualizarCadastro(UpdateView):
+    model = Pessoas
+    fields = ['nome','idade','nacionalidade','profissao']
+    template_name ='atualizar.html'
+    success_url ='/pessoas/'
+
+
+class DeletarCadastro(DeleteView):
+    model = Pessoas
+    template_name = 'deletar.html'
+    success_url = reverse_lazy('pessoas')
+    
+    
